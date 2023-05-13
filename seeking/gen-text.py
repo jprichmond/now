d8e5b60c986053b58f07c8cdff4ffe43129bf64a
Jason <jprichmond@alumni.iu.edu>
2023-05-13 12:11:24 -0400

    tweaks

--- seeking/gen-text.py
+++ seeking/gen-text.py
@@ -87,6 +87,6 @@ output = open('../seeking-text.html', 'w');output.write(f'''\
 <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="style.css">
-<body class="content"><pre>
+<body class="preContent"><pre>
 {text}
 </pre>''')
